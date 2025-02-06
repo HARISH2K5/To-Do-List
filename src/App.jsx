0d35b225bd55fc601d2bae085f0a6f57455a8b23
HARISH2K5 <harishr.cs23@gail.com>
2025-02-06 10:05:06 +0530

    Your commit message

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>TO DO</h1>
+        <div>
+          <input type="text" />
+          <button>add</button>
+        </div>
+        
+    </div>
+  )
+}
+
+export default App
