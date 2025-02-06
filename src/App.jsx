5695b299d11c82b10ac8c8fa77180c780450b914
Harish <166651036+HARISH2K5@users.noreply.github.com>
2025-02-06 10:11:00 +0530

    added to do list

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,69 @@ function App() {
   return (
     <div>
       <h1>TO DO</h1>
-        <div>
-          <input type="text" />
-          <button>add</button>
-        </div>
+        import React, { useState } from 'react'
+
+function NewApp() {
+    const [items, setItems] = useState([
+      ]);
+    let [newItem, setNewItem] = useState("")
+    
+    const handleChecked = (id) => {
+        let newListItems = items.map((item) =>
+          item.id === id ? { ...item, checked: !item.checked } : item
+        );
+        
+        setItems(newListItems);
+      };
+      const handleDelete = (id) => {
+        let newItems = items.filter((item) => item.id !== id);
+        setItems(newItems);
+      };
+      const addItem = () => {
+        if (newItem === "") return;
+        setItems([
+            ...items, 
+            { id: items.length + 1, label: newItem, checked: false }
+        ]);
+        setNewItem("");
+    };
+  return (
+    <>
+    <div>
+        <input type="text" 
+        value={ newItem } 
+        placeholder="Add New Item"
+        onChange={(e)=>{
+            setNewItem(e.target.value)
+        }}
+        onClick={()=>{
+            add
+        }}
+        />
+        <button on onClick={()=>{
+            addItem()
+        }}>Add</button>
+    </div>
+    
+    <ul>
+          {items.map((item) => (
+            <li key={item.id} className='item'>
+              <input
+              type="checkbox"
+              checked={item.checked}
+              onChange={() => handleChecked(item.id)}
+            />
+              <label>{item.label}</label>
+              <button onClick={() => handleDelete(item.id)}>Delete</button>
+            </li>
+          ))}
+    </ul>
+    </>
+        
+      );
+}
+
+export default NewApp
         
     </div>
   )
